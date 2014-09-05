4e2efe4857b572012e3fca2353cf157de7a77946
alex <o4kan@ya.ru>
2014-09-05 14:16:19 +0400

    redesign finish

--- views/home.js
+++ views/home.js
@@ -4,12 +4,7 @@ Taxi1.home = function(params) {
 
     var dis_phone = Taxi1.config.dis_phone;
 
-    function call(){
-        $('.tel_call').click();
-    }
-
     return {
         phone: 'tel:' + dis_phone,
-        call: call,
     };
 };
