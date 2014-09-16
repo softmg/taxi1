e199f0d602a97c46adda21b6e362f8f0eb0da004
alex <o4kan@ya.ru>
2014-09-16 18:58:38 +0400

    android remove getTag function

--- PushAndroid.js
+++ PushAndroid.js
@@ -19,7 +19,7 @@ function registerPushAndroid() {
             //and show alert
             navigator.notification.alert(title);
             //stopping geopushes
-            pushNotification.stopGeoPushes();
+            //pushNotification.stopGeoPushes();
         }
     );
 
@@ -105,7 +105,6 @@ function registerPushAndroid() {
 
 function onPushAndroidInitialized(pushToken)
 {
-
     //output the token to the console
     console.warn('push token: ' + pushToken);
     store.insert({
@@ -116,40 +115,4 @@ function onPushAndroidInitialized(pushToken)
     _sendToken(pushToken);
 
     var pushNotification = window.plugins.pushNotification;
-    /*pushNotification.getTags(
-        function(tags)
-        {
-            console.warn('tags for the device: ' + JSON.stringify(tags));
-        },
-        function(error)
-        {
-            console.warn('get tags error: ' + JSON.stringify(error));
-        }
-    );*/
-    //set multi notificaiton mode
-    //pushNotification.setMultiNotificationMode();
-    //pushNotification.setEnableLED(true);
-    //set single notification mode
-    //pushNotification.setSingleNotificationMode();
-    //disable sound and vibration
-    //pushNotification.setSoundType(1);
-    //pushNotification.setVibrateType(1);
-    //pushNotification.setLightScreenOnNotification(false);
-    //goal with count
-    //pushNotification.sendGoalAchieved({goal:'purchase', count:3});
-    //goal with no count
-    //pushNotification.sendGoalAchieved({goal:'registration'});
-    //setting list tags
-    //pushNotification.setTags({"MyTag":["hello", "world"]});
-    //settings tags
-    /*pushNotification.setTags({deviceName:"hello", deviceId:10},
-        function(status) {
-            console.warn('setTags success');
-        },
-        function(status) {
-            console.warn('setTags failed');
-        }
-    );*/
-    //Pushwoosh Android specific method that cares for the battery
-    pushNotification.startGeoPushes();
 }
