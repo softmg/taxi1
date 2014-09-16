0b84746ad29f5112a90802fa715a2145e77a623e
alex <o4kan@ya.ru>
2014-09-16 23:51:40 +0400

    change weinre address

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var is_mobile = false;
 
 if(DevExpress.devices && DevExpress.devices.current() && DevExpress.devices.current().platform !== 'generic')
 {
-    is_mobile = false; //set false for emulator debug
+    is_mobile = true; //set false for emulator debug
 }
 
 var mycallback = function(data)
