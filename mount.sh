81a6e2430ef8a2581daf47456301fc170c32955d
alex <o4kan@ya.ru>
2014-09-24 00:04:41 +0400

    finish ver 0.1.0

--- mount.sh
+++ mount.sh
@@ -3,4 +3,4 @@ sudo rm -rf /var/www/phonegap/lib/iphone4/
 sudo mkdir /var/www/phonegap/lib/iphone4/
 sudo chown alex:alex /var/www/phonegap/lib/iphone4/
 ifuse /var/www/phonegap/lib/iphone4/ --root
-mv ~/Загрузки/Taxi1.ipa /var/www/phonegap/lib/iphone4/var/mobile/Downloads/Taxi1.ipa
+mv ~/Загрузки/AppleTaxi.ipa /var/www/phonegap/lib/iphone4/var/mobile/Downloads/AppleTaxi.ipa
